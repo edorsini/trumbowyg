e4b58c08f60453f66f67bb231248bf067b8242ca
Ed Orsini <edorsini@Eds-MacBook-Pro.local>
2019-04-05 14:44:02 -0400

    fixing for deployment

--- public/js/site.js
+++ public/js/site.js
@@ -1,30 +1,32 @@
 $(function() {
 
-$.ajax({
-  type: "GET",
-  url: "https://trumbowyg-api.herokuapp.com/api/templates",
-  success: function(data){
-     $("body").text(data);
+  function getTemplates() {
+    var templates = [];
+    
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        type: "GET",
+        url: "https://trumbowyg-api.herokuapp.com/api/templates",
+        success: function(data){
+          //console.log(data); 
+          resolve(data);
+          //$("body").text(data);
+        }
+      });
+    });
+    return templates;
   }
-});
 
-
-  $('#trumbowyg-demo')
-  .trumbowyg({
+  async function init() {
+    $('#trumbowyg-demo')
+    .trumbowyg({
       btns: ['mittemplate'],
       plugins: {
-          templates: [
-              {
-                  name: 'Template 1',
-                  html: '<p>I am a template!</p>'
-              },
-              {
-                  name: 'Template 2',
-                  html: '<p>I am a different template!</p>'
-              }
-          ]
+        templates: await getTemplates()
       }
-  });
+    });
+  }
 
+ init();
 
 });
--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public/'));
-app.use('/trumbowyg', express.static(__dirname + '/node_modules/trumbowyg/dist/'));
+//app.use('/trumbowyg', express.static(__dirname + '/node_modules/trumbowyg/dist/'));
 
 
 app.listen(port, function() {
