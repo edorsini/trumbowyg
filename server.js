8212e3d4646c120466218a1398d3ffb63c162e22
Ed Orsini <edorsini@Eds-MacBook-Pro.local>
2019-04-05 13:48:52 -0400

    tweaking

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
-var port = 8080;
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public/'));
 app.use('/trumbowyg', express.static(__dirname + '/node_modules/trumbowyg/dist/'));
